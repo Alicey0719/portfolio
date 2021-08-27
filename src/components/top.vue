a1c29406ab2b476d3e4e07a610901f0eccbbdadc
Alicey <sirius@alicey.dev>
2021-08-28 05:07:24 +0900

    [lean] card

--- src/components/top.vue
+++ src/components/top.vue
@@ -1,3 +1,73 @@
 <template>
-    <h1>top</h1>
+<div class="content-area container">
+    <div class="row">
+        <div class="col">        
+            <a href="#">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://alicey.dev/img/icon2.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col">        
+            <a href="#">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://alicey.dev/img/icon2.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col">        
+            <a href="#">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://alicey.dev/img/icon2.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col">        
+            <a href="#">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://alicey.dev/img/icon2.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col">        
+            <a href="#">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://alicey.dev/img/icon2.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col">        
+            <a href="#">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://alicey.dev/img/icon2.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+
+    </div>
+</div>
 </template>
