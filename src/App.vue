284e491a9cd1c001414316e38bccaf1c1a8370fc
Alicey <sirius@alicey.dev>
2021-08-28 04:00:29 +0900

    [update] add css

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="app">
     <mainHeader/>
-    <img src="./assets/logo.png">
     <router-view/>
-    <button class="btn btn-primary">test</button>
   </div>
 </template>
 
@@ -19,12 +17,5 @@ export default {
 </script>
 
 <style>
-#app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
+@import 'css/main.css';
 </style>
