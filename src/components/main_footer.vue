ef54baad8bfe9f6428ae927eb7eba20dcc7a9784
Alicey <sirius@alicey.dev>
2021-08-29 06:12:25 +0900

    [update] add footer

---
+++ src/components/main_footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer class="footer">
+        <div class="container footer-container text-center">
+            <p class="text-muted">Copyright 2021 Alicey.</p>
+        </div>
+    </footer>
+</template>
