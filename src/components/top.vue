174573590441765f3d6adfdadfef53e0d2974aec
Alicey <sirius@alicey.dev>
2021-09-09 16:22:59 +0900

    [update] axios, json取得 の実装

--- src/components/top.vue
+++ src/components/top.vue
@@ -1,73 +1,39 @@
 <template>
 <div class="content-area container">
     <div class="row">
-        <div class="col mt-5">        
+
+        <div class="col mt-5" v-for="item in products" :key="item.name" >        
             <a class="content-link" href="#">
                 <div class="card" style="width: 18rem;">
                     <img src="https://alicey.dev/img/icon2.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p class="card-text">{{item.name}}</p>
                     </div>
                 </div>
             </a>
+            
         </div>
-
-        <div class="col mt-5">        
-            <a class="content-link" href="#">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://alicey.dev/img/icon2.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-            </a>
-        </div>
-
-        <div class="col mt-5">        
-            <a class="content-link" href="#">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://alicey.dev/img/icon2.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-            </a>
-        </div>
-
-        <div class="col mt-5">        
-            <a class="content-link" href="#">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://alicey.dev/img/icon2.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-            </a>
-        </div>
-
-        <div class="col mt-5">        
-            <a class="content-link" href="#">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://alicey.dev/img/icon2.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-            </a>
-        </div>
-
-        <div class="col mt-5">        
-            <a class="content-link" href="#">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://alicey.dev/img/icon2.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-            </a>
-        </div>
-
-
     </div>
 </div>
 </template>
+
+
+<script>
+export default {
+    data () {
+        return{
+            products : null
+        }
+    },
+    methods:{
+        async getProducts() {
+            var url = '/static/doc/product-list.json'
+            await this.axios.get(url).then(x => { this.products = x.data })
+            console.log(this.products)
+        }
+    },
+    mounted(){
+        this.getProducts()
+    }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import Router from 'vue-router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import top from '@/components/top'
 import tech from '@/components/tech'
-
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import '@/css/main.css'
 
 Vue.use(Router)
 Vue.use(BootstrapVue)
+Vue.use(VueAxios, axios)
 
 export default new Router({
     routes: [{
