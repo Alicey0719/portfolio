41244454e8b56385595be3d6d1d6128f1fe5f27b
Alicey <sirius@alicey.dev>
2021-09-15 19:52:55 +0900

    [update] 値取得, フィルタ構造

--- src/components/top.vue
+++ src/components/top.vue
@@ -1,20 +1,29 @@
 <template>
 <div class="content-area container">
-    <div class="row  product">
-        <div class="col mt-5" v-for="item in products" :key="item.name" >        
-            <a class="content-link" target="_blank" v-bind:href="item.url">
-                <div class="card" style="width: 18rem;">
-                    <img v-bind:src="item.img" decoding="async" class="card-img-top" v-bind:alt="item.name">
-                    <div class="card-body">
-                        <p class="card-title">{{item.name}}</p>
-                        <p class="card-form">開発形態 : {{item.form}}</p>
-                        <p class="card-period">開発期間 : {{item.period}}</p>
-                        <div class="card-partition"></div>
-                        <p class="card-text">{{item.dist}}</p>
-                    </div>
-                </div>
-            </a>
+    <div class="row product">
+
+        <div>
+            <h2>Programming</h2>
         </div>
+        <div class="row">
+            <div class="col mt-5" v-for="item in getProgrammings" :key="`programming-${item.name}`" >
+                <a class="content-link" target="_blank" v-bind:href="item.url">
+                    <div class="card" style="width: 18rem;">
+                        <img v-bind:src="item.img" decoding="async" class="card-img-top" v-bind:alt="item.name">
+                        <div class="card-body">
+                            <p class="card-title">{{item.name}}</p>
+                            <p class="card-form">開発形態 : {{item.form}}</p>
+                            <p class="card-period">開発期間 : {{item.period}}</p>
+                            <div class="card-partition"></div>
+                            <p class="card-text">{{item.dist}}</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+
+
     </div>
 </div>
 </template>
@@ -29,13 +38,26 @@ export default {
     },
     methods:{
         async getProducts() {
-            var url = '/static/doc/product-list.json'
-            await this.axios.get(url).then(x => { this.products = x.data })
-            //console.log(this.products)
+            const url = '/static/doc/product-list.json';
+            await this.axios.get(url).then(x => { this.products = x.data });
         }
     },
     mounted(){
         this.getProducts()
+    },
+    computed: {
+        getProgrammings: function(){
+            if (this.products != null){
+                let res = Array.from(this.products);
+                res = res.filter(function(v){
+                    return v.category === 'programming'
+                });
+                //console.log(res);
+                return res;
+            }else{
+                return null;
+            }
+        }
     }
 }
 </script>
