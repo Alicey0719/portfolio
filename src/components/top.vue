47d88f7b304d47cb5468663cf2912718fe34b408
Alicey <sirius@alicey.dev>
2021-09-15 20:19:55 +0900

    [update] top カテゴリ別表示

--- src/components/top.vue
+++ src/components/top.vue
@@ -7,8 +7,8 @@
         </div>
         <div class="row">
             <div class="col mt-5" v-for="item in filterProducts('programming')" :key="`programming-${item.name}`" >
-                <a class="content-link" target="_blank" v-bind:href="item.url">
-                    <div class="card" style="width: 18rem;">
+                <div class="card" style="width: 18rem;">
+                    <a class="content-link" target="_blank" v-bind:href="item.url">
                         <img v-bind:src="item.img" decoding="async" class="card-img-top" v-bind:alt="item.name">
                         <div class="card-body">
                             <p class="card-title">{{item.name}}</p>
@@ -17,8 +17,68 @@
                             <div class="card-partition"></div>
                             <p class="card-text">{{item.dist}}</p>
                         </div>
-                    </div>
-                </a>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div>
+            <h2>Server</h2>
+        </div>
+        <div class="row">
+            <div class="col mt-5" v-for="item in filterProducts('server')" :key="`server-${item.name}`" >
+                <div class="card" style="width: 18rem;">
+                    <a class="content-link" target="_blank" v-bind:href="item.url">
+                        <img v-bind:src="item.img" decoding="async" class="card-img-top" v-bind:alt="item.name">
+                        <div class="card-body">
+                            <p class="card-title">{{item.name}}</p>
+                            <p class="card-form">開発形態 : {{item.form}}</p>
+                            <p class="card-period">開発期間 : {{item.period}}</p>
+                            <div class="card-partition"></div>
+                            <p class="card-text">{{item.dist}}</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div>
+            <h2>Movie</h2>
+        </div>
+        <div class="row">
+            <div class="col mt-5" v-for="item in filterProducts('movie')" :key="`movie-${item.name}`" >
+                <div class="card" style="width: 18rem;">
+                    <a class="content-link" target="_blank" v-bind:href="item.url">
+                        <img v-bind:src="item.img" decoding="async" class="card-img-top" v-bind:alt="item.name">
+                        <div class="card-body">
+                            <p class="card-title">{{item.name}}</p>
+                            <p class="card-form">開発形態 : {{item.form}}</p>
+                            <p class="card-period">開発期間 : {{item.period}}</p>
+                            <div class="card-partition"></div>
+                            <p class="card-text">{{item.dist}}</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div>
+            <h2>Other</h2>
+        </div>
+        <div class="row">
+            <div class="col mt-5" v-for="item in filterProducts('other')" :key="`other-${item.name}`" >
+                <div class="card" style="width: 18rem;">
+                    <a class="content-link" target="_blank" v-bind:href="item.url">
+                        <img v-bind:src="item.img" decoding="async" class="card-img-top" v-bind:alt="item.name">
+                        <div class="card-body">
+                            <p class="card-title">{{item.name}}</p>
+                            <p class="card-form">開発形態 : {{item.form}}</p>
+                            <p class="card-period">開発期間 : {{item.period}}</p>
+                            <div class="card-partition"></div>
+                            <p class="card-text">{{item.dist}}</p>
+                        </div>
+                    </a>
+                </div>
             </div>
         </div>
 
