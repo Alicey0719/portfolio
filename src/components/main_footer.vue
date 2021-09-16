85ca1a6bf90d84183e1d371ca507f030c745c39d
Alicey <sirius@alicey.dev>
2021-09-17 01:39:05 +0900

    [update] pagetop

--- src/components/main_footer.vue
+++ src/components/main_footer.vue
@@ -1,7 +1,10 @@
 <template>
+<div>
     <footer class="footer">
         <div class="container footer-container text-center">
             <p class="text-muted">Copyright 2021 Alicey.</p>
         </div>
     </footer>
+    <div id="pagetop"><a href="#top"><i class="fas fa-angle-up"></i></a></div>
+</div>
 </template>
